4ac619a08e37d16b2159000bc44df2ee41c40af6
yoelq1 <149291230+yoelq1@users.noreply.github.com>
2025-09-06 01:00:34 +0700

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+// js/supabase.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const SUPABASE_URL = "https://zvqlsgwccrdqjgcxgmzq.supabase.co"; // <- ganti
+const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2cWxzZ3djY3JkcWpnY3hnbXpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcwNTc0MDUsImV4cCI6MjA3MjYzMzQwNX0.6Ge1ON_x9Ce-l4tFRtH_Ks9o3v1RouLIDejtbohjo4Y"; // <- ganti
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON);
